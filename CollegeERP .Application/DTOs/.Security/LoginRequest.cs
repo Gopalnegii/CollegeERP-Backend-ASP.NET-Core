653e434755a385fc785a7518469c6f5881f95084
Suraj Negi <sn945626@gmail.com>
2026-01-26 02:52:25 +0530

    login dtos added for Authentication

---
+++ CollegeERP .Application/DTOs/.Security/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CollegeERP_.Application.DTOs.Security
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
+        public string Email { get; set; } = null!;
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ CollegeERP .Application/DTOs/.Security/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollegeERP_.Application.DTOs.Security
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = null!;
+        public int UserId { get; set; }
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
